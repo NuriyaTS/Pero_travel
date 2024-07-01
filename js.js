a0b7d1ed9a75fc000cec4a8df31aedef71f395a9
Nuriya <boniblueright@gmail.com>
2024-07-01 09:21:16 +0600

    add existing file

--- js.js
+++ js.js
@@ -1,12 +1,14 @@
 var swiper = new Swiper(".mySwiper", {
   effect: "cube",
   grabCursor: true,
+  // loop: true,
   cubeEffect: {
     shadow: true,
     slideShadows: true,
     shadowOffset: 10,
     shadowScale: 0.94,
   },
+
   autoplay: {
     delay: 5000,
     disableOnInteraction: false,
@@ -14,8 +16,25 @@ var swiper = new Swiper(".mySwiper", {
 });
 var swiper = new Swiper(".mySecondSwiper", {
   slidesPerView: 3,
-  spaceBetween: 0,
+  spaceBetween: 10,
   freeMode: true,
+  // loop: true,
+  breakpoints: {
+    900: {
+      slidesPerView: 3,
+      spaceBetween: 0,
+    },
+    700: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+      loop: true
+    },
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 10,
+      loop: true
+    }
+  },
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
@@ -27,14 +46,31 @@ var swiper = new Swiper(".mySecondSwiper", {
 });
 var swiper = new Swiper(".myFeedSwiper", {
   slidesPerView: 2,
-  spaceBetween: 30,
+  spaceBetween: 10,
   freeMode: true,
+  loop: true,
+  breakpoints:{
+  700: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+      loop:true
+    },
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 10,
+      loop:true
+    }
+  },
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
-  },
-  autoplay: {
-    delay: 5000,
-    disableOnInteraction: false,
   }
 });
+function showBurger() {
+  let ul = document.querySelector('header ul');
+  ul.classList.toggle('active');
+  let brg = document.querySelector('header .burger');
+  brg.classList.toggle('active');
+  let bef = document.querySelector('header .burger::before');
+  bef.classList.toggle('active');
+};
